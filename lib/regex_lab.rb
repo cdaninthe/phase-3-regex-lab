88e51b2eed604e47c09239346ba2be6e667021fe
Cecilia Daninthe <cecilia.daninthe@gmail.com>
2023-03-14 16:54:34 -0400

    commit tree bf05566702f45d3c88aefcca598c44419034a6e8

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,17 +1,17 @@
 def starts_with_a_vowel?(word)
-
+    word.scan(/^[aeiou]/) ? true : false
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+    text.scan(/un.ing/)
 end
 
 def words_five_letters_long(text)
-
+    text.scan(/^\w{5}\s/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+    text.match(/\w[A-Z]*[?.!]$/) ? true : false
 end
 
 def valid_phone_number?(phone)
